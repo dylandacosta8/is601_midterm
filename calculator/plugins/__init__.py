973f14e29fa25a603aba0e5e3ebf0f27089627c1
Dylan Da Costa <dylan2222@gmail.com>
2024-10-16 10:43:54 -0400

    moved to using a Calculator instance

--- calculator/plugins/__init__.py
+++ calculator/plugins/__init__.py
@@ -1,24 +1,25 @@
+from calculator import Calculator 
 import importlib
 import pkgutil
 import os
 import logging
-
 from abc import ABC, abstractmethod
 
 logger = logging.getLogger('calculator_app')
 
 class Command(ABC):
     @abstractmethod
-    def execute(self, a, b):
-        pass #TODO
+    def execute(self, a=None, b=None):  # Adjusted to accept optional parameters for history commands
+        pass
 
     @abstractmethod
     def help(self):
-        pass #TODO
+        pass
 
 class PluginManager:
     def __init__(self):
         self.plugins = {}
+        self.calculator = Calculator()  # Create an instance of Calculator
 
     def load_plugins(self):
         # Path to the plugins directory
@@ -33,13 +34,15 @@ class PluginManager:
 
                 # Get the command class (assumed to be named <ModuleName>Command)
                 command_class = getattr(module, f"{module_name.capitalize()}Command")
-                command_instance = command_class()
+
+                # Create an instance of the command class with the calculator instance
+                command_instance = command_class(self.calculator)
 
                 # Use the module name as the command key
                 self.plugins[module_name] = command_instance
                 logger.info(f"Successfully loaded plugin: {module_name}")
-            except Exception as e: #TODO
-                logger.error(f"Failed to load command '{module_name}': {e}") #TODO
+            except Exception as e:
+                logger.error(f"Failed to load command '{module_name}': {e}")
 
     def list_command_modules(self, package):
         # Returns a list of command modules in the specified package
@@ -47,19 +50,19 @@ class PluginManager:
             command_modules = [name for _, name, _ in pkgutil.iter_modules([package.replace('.', os.sep)])]
             logger.debug(f"Found command modules: {command_modules}")
             return command_modules
-        except Exception as e: #TODO
-            logger.exception(f"Error listing command modules in package '{package}': {e}") #TODO
-            return [] #TODO
+        except Exception as e:
+            logger.exception(f"Error listing command modules in package '{package}': {e}")
+            return []
 
     def get_command(self, command_name):
         command = self.plugins.get(command_name)
         if command:
             logger.debug(f"Retrieved command: {command_name}")
         else:
-            logger.warning(f"Command not found: {command_name}") #TODO
+            logger.warning(f"Command not found: {command_name}")
         return command
 
     def list_plugins(self): 
-        plugin_keys = list(self.plugins.keys()) #TODO
-        logger.debug(f"Listing loaded plugins: {plugin_keys}") #TODO
-        return plugin_keys #TODO
+        plugin_keys = list(self.plugins.keys())
+        logger.debug(f"Listing loaded plugins: {plugin_keys}")
+        return plugin_keys
