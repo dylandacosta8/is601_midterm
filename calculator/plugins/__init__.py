7e5e775c64ab003ed7c4fabdba2e84e494104e0e
Dylan Da Costa <dylan2222@gmail.com>
2024-10-18 02:49:17 -0400

    initialiazed calculator instance

--- calculator/plugins/__init__.py
+++ calculator/plugins/__init__.py
@@ -1,4 +1,3 @@
-from calculator import Calculator 
 import importlib
 import pkgutil
 import os
@@ -7,35 +6,24 @@ import logging
 logger = logging.getLogger('calculator_app')
 
 class PluginManager:
-    def __init__(self):
+    def __init__(self, calculator):
         self.plugins = {}
-        self.calculator = Calculator()  # Create an instance of Calculator
+        self.calculator = calculator
 
     def load_plugins(self):
-        # Path to the plugins directory
         plugins_dir = "calculator.plugins"
-
-        # Automatically discover and load command plugins
         for module_name in self.list_command_modules(plugins_dir):
             try:
-                # Import the module
                 logger.debug(f"Attempting to import module: {module_name}")
                 module = importlib.import_module(f"{plugins_dir}.{module_name}")
-
-                # Get the command class (assumed to be named <ModuleName>Command)
                 command_class = getattr(module, f"{module_name.capitalize()}Command")
-
-                # Create an instance of the command class with the calculator instance
                 command_instance = command_class(self.calculator)
-
-                # Use the module name as the command key
                 self.plugins[module_name] = command_instance
                 logger.info(f"Successfully loaded plugin: {module_name}")
             except Exception as e:
                 logger.error(f"Failed to load command '{module_name}': {e}")
 
     def list_command_modules(self, package):
-        # Returns a list of command modules in the specified package
         try:
             command_modules = [name for _, name, _ in pkgutil.iter_modules([package.replace('.', os.sep)])]
             logger.debug(f"Found command modules: {command_modules}")
@@ -52,7 +40,7 @@ class PluginManager:
             logger.warning(f"Command not found: {command_name}")
         return command
 
-    def list_plugins(self): 
+    def list_plugins(self):
         plugin_keys = list(self.plugins.keys())
         logger.debug(f"Listing loaded plugins: {plugin_keys}")
         return plugin_keys
